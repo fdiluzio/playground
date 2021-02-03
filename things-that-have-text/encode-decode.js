e1452ce7c1db123b1662478c12de1a03cc91d0dc
Frank.Di.Luzio <frank.di.luzio@intrafind.de>
2021-02-03 10:55:31 +0100

    remove script tags

--- things-that-have-text/encode-decode.js
+++ things-that-have-text/encode-decode.js
@@ -1,12 +1,11 @@
-<script type="text/javascript">
-  function encode() {
-    var obj = document.getElementById('dencoder');
-    var unencoded = obj.value;
-    obj.value = encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");
-  }
+function encode() {
+  var obj = document.getElementById('dencoder');
+  var unencoded = obj.value;
+  obj.value = encodeURIComponent(unencoded).replace(/'/g, "%27").replace(/"/g, "%22");
+}
+
 function decode() {
   var obj = document.getElementById('dencoder');
   var encoded = obj.value;
-  obj.value = decodeURIComponent(encoded.replace(/\+/g,  " "));
+  obj.value = decodeURIComponent(encoded.replace(/\+/g, " "));
 }
-</script>
